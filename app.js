ec698f178de47883393f92e4d174e8bd1739c74f
Rafael <rafinha_fern@hotmail.com>
2022-04-25 14:57:56 -0300

    back-end recebendo informação de log do usuário, guardando postagem de tweets e filtrando os ultimos 10 para entrega ao front, finalizando projeto

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 import express from 'express';
 import cors from 'cors';
+
 const app = express();
 app.use(cors());
-app.get("/", (req, res) => {
-    res.send('Hello World');
+app.use(express.json());
+let usernameLog = '';
+let avatarLog = '';
+let tweets = [{
+	username: "bobesponja",
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  tweet: "eu amo o hub",
+}];
+function ultimos10() {
+    if(tweets.length<=10) {
+        return tweets;
+    }
+    if(tweets.length>10) {
+        let ultimosTweets = [];
+        for(let i = 0;i<10;i++){
+            ultimosTweets.push(tweets[i])
+        }
+        return ultimosTweets;
+    }
+}
+app.post("/sign-up",(req,res) => {
+    let user = req.body;
+     usernameLog=user.username;
+     avatarLog=user.avatar;
+    res.send("OK");
 });
-app.listen(4000);
+app.post("/tweets",(req,res)=>{
+    let tweet = req.body.tweet;
+    let tweetLog = {
+        username:usernameLog,
+        avatar:avatarLog,
+        tweet:tweet
+    }
+    tweets = [tweetLog,...tweets];
+    res.send("OK");
+});
+app.get("/tweets", (req, res) => {
+    res.send(ultimos10());
+});
+app.listen(5000);
