5ded5137af36da86a8e3f680f5d4926ba7994184
Rafael <rafinha_fern@hotmail.com>
2022-04-25 00:58:25 -0300

    Configuração e operação do back-end inicial

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+app.get("/", (req, res) => {
+    res.send('Hello World');
+});
+app.listen(4000);
