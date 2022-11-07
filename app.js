199c371944823cb050a875231a58042914f5fd42
Rafael <rafinha_fern@hotmail.com>
2022-11-07 14:25:07 -0300

    feat: new env to port and http route, docker and docker-compose files, first reame template

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import {env} from 'node:process';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+app.use('/images',express.static('images'));
+
 let usernameLog = '';
 let avatarLog = '';
+let http = env.HTTP;
 let tweets = [{
-	username: "bobesponja",
-	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-  tweet: "eu amo o hub",
+	username: "Sandy Cheeks",
+	avatar: `${http}images/sandy-cheeks.jpg`,
+    tweet: "Hellow, good to see you here. You are awesome.",
 }];
+
 function ultimos10() {
     if(tweets.length<=10) {
         return tweets;
@@ -23,12 +29,14 @@ function ultimos10() {
         return ultimosTweets;
     }
 }
+
 app.post("/sign-up",(req,res) => {
     let user = req.body;
      usernameLog=user.username;
      avatarLog=user.avatar;
     res.send("OK");
 });
+
 app.post("/tweets",(req,res)=>{
     let tweet = req.body.tweet;
     let tweetLog = {
@@ -39,7 +47,12 @@ app.post("/tweets",(req,res)=>{
     tweets = [tweetLog,...tweets];
     res.send("OK");
 });
+
 app.get("/tweets", (req, res) => {
     res.send(ultimos10());
+
 });
-app.listen(5000);
+
+app.listen(env.PORT, ()=> {
+    console.log(`Hello i'm running on port = ${env.PORT}`)
+});
